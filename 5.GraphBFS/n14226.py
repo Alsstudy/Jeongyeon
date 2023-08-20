19f31c3364c74f05e463cb2b6c169d6f6d4a8558
jyeon <jeongyeon865@gmail.com>
2023-08-20 21:27:43 +0900

    [ADD] 08/20

--- 5.GraphBFS/n14226.py
+++ 5.GraphBFS/n14226.py
@@ -4,20 +4,34 @@ input = sys.stdin.readline
 
 
 def bfs():
-    while queue:
-        nidx = queue.popleft()
-        if nidx == s:
-            print(li[s])
-            return
-        for i in [0, nidx, nidx-1]:
-            if 0 <= i <= 1000:
-                if li[i] == 0:
-                    li[i] = li[nidx] + 1
+    while q:
+        s, c = q.popleft()
+        if dist[s][s] == -1:  # 붙여넣기
+            dist[s][s] = dist[s][c] + 1
+            q.append((s, s))
+        if s + c <= n and dist[s + c][c] == -1:  # 복사
+            dist[s + c][c] = dist[s][c] + 1
+            q.append((s + c, c))
+        if s - 1 >= 0 and dist[s - 1][c] == -1:  # 삭제
+            dist[s - 1][c] = dist[s][c] + 1
+            q.append((s - 1, c))
 
 
-s = int(input())
-li = [0] * 1001
+n = int(input())
 
-queue = deque([1])
+dist = [[-1] * (n + 1) for _ in range(n + 1)]
+q = deque()
+q.append((1, 0))
+
+dist[1][0] = 0
 
 bfs()
+
+answer = -1
+
+for i in range(n + 1):
+    if dist[n][i] != -1:
+        if answer == -1 or answer > dist[n][i]:
+            answer = dist[n][i]
+
+print(answer)
