72c8e5f092d7641d084c282975c43d221a814453
jyeon <jeongyeon865@gmail.com>
2023-08-30 18:21:50 +0900

    [ADD] 08/30

--- 6.Simulation/n14499.py
+++ 6.Simulation/n14499.py
@@ -1,38 +1,36 @@
-# undo
 import sys
 input = sys.stdin.readline
 
-dr = [0, 0, 0, -1, 1]
-dc = [0, 1, -1, 0, 0]
-
 n, m, x, y, k = map(int, input().split())
-maps = [list(map(int, input().split())) for _ in range(n)]
+li = [list(map(int, input().split())) for _ in range(n)]
 command = list(map(int, input().split()))
+
+move = [(0, 0), (0, 1), (0, -1), (-1, 0), (1, 0)]
 dice = [0] * 6
 
 for d in command:
-    nx = x + dr[d]
-    ny = y + dc[d]
+    nx = x + move[d][0]
+    ny = y + move[d][1]
 
     if not 0 <= nx < n or not 0 <= ny < m:
         continue
 
     east, west, south, north, up, down = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
 
-    if d == 1:
+    if d == 1:  # 동
         dice[0], dice[1], dice[4], dice[5] = down, up, east, west
-    elif d == 2:
+    elif d == 2:  # 서
         dice[0], dice[1], dice[4], dice[5] = up, down, west, east
-    elif d == 3:
+    elif d == 3:  # 북
         dice[2], dice[3], dice[4], dice[5] = up, down, north, south
-    elif d == 4:
+    elif d == 4:  # 남
         dice[2], dice[3], dice[4], dice[5] = down, up, south, north
 
-    if maps[nx][ny] == 0:
-        maps[nx][ny] = dice[5]
+    if li[nx][ny] == 0:
+        li[nx][ny] = dice[5]
     else:
-        dice[5] = maps[nx][ny]
-        maps[nx][ny] = 0
+        dice[5] = li[nx][ny]
+        li[nx][ny] = 0
 
-    x, y = nx, ny
     print(dice[4])
+    x, y = nx, ny
