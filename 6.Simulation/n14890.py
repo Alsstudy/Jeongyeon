e3d3974aaed7db11ff2b7c1a190bc7934ab834af
jyeon <jeongyeon865@gmail.com>
2023-09-04 22:18:03 +0900

    [ADD] 08/30

--- 6.Simulation/n14890.py
+++ 6.Simulation/n14890.py
@@ -1,36 +1,42 @@
 import sys
 input = sys.stdin.readline
 
-n, m, x, y, k = map(int, input().split())
-li = [list(map(int, input().split())) for _ in range(n)]
-command = list(map(int, input().split()))
 
-move = [(0, 0), (0, 1), (0, -1), (-1, 0), (1, 0)]
-dice = [0] * 6
+def check_line(line):
+    for i in range(1, n):
+        if abs(line[i] - line[i-1]) > 1:
+            return False
 
-for d in command:
-    nx = x + move[d][0]
-    ny = y + move[d][1]
+        if line[i] < line[i-1]:
+            for j in range(l):
+                if i + j >= n or line[i] != line[i+j] or slope[i+j]:
+                    return False
+                if line[i] == line[i+j]:
+                    slope[i+j] = True
 
-    if not 0 <= nx < n or not 0 <= ny < m:
-        continue
+        elif line[i] > line[i-1]:
+            for j in range(l):
+                if i-j-1 < 0 or line[i-1] != line[i-j-1] or slope[i-j-1]:
+                    return False
+                if line[i-1] == line[i-j-1]:
+                    slope[i-j-1] = True
 
-    east, west, south, north, up, down = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
+    return True
 
-    if d == 1:  # 동
-        dice[0], dice[1], dice[4], dice[5] = down, up, east, west
-    elif d == 2:  # 서
-        dice[0], dice[1], dice[4], dice[5] = up, down, west, east
-    elif d == 3:  # 북
-        dice[2], dice[3], dice[4], dice[5] = up, down, north, south
-    elif d == 4:  # 남
-        dice[2], dice[3], dice[4], dice[5] = down, up, south, north
 
-    if li[nx][ny] == 0:
-        li[nx][ny] = dice[5]
-    else:
-        dice[5] = li[nx][ny]
-        li[nx][ny] = 0
+n, l = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-    print(dice[4])
-    x, y = nx, ny
+ans = 0
+
+for i in range(n):
+    slope = [False] * n
+    if check_line(arr[i]):
+        ans += 1
+
+for j in range(n):
+    slope = [False] * n
+    if check_line([arr[i][j] for i in range(n)]):
+        ans += 1
+
+print(ans)
