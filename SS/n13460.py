e4eb83f72f219fc08b7e526acda9c2ba172ac6b4
jyeon <jeongyeon865@gmail.com>
2023-09-13 12:03:27 +0900

    [ADD] 09/13

--- SS/n13460.py
+++ SS/n13460.py
@@ -1,43 +1,52 @@
-def dfs(nidx, midx, cnt):
-    if arr[nidx][midx] == '0':
-        return cnt
+def bfs(ridx, bidx):
+    q = []
+    q.append(ridx)
+    cnt = 1
+    nbi = bidx[0]
+    nbj = bidx[1]
+    mark = False
 
-    for i in range [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        nidx += i[0]
-        midx += i[1]
-        if 0 <= nidx < n and 0 <= midx < m:
-            if arr[nidx][midx] != '#':
-                dfs(nidx, midx, cnt+1)
+    while q:
+        nidx = q.pop(0)
+        nri = nidx[0]
+        nrj = nidx[1]
+        visited[nri][nrj] = 1
 
+        for i in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            bi = nbi
+            bj = nbj
+            while 0 <= bi + i[0] < n and 0 <= bj + i[1] < m and arr[bi + i[0]][bj + i[1]] != '#' and arr[bi + i[0]][bj + i[1]] != 'R':
+                bi += i[0]
+                bj += i[1]
+                if arr[bi][bj] == 'O':
+                    return -1
 
-# def move(dir, nidx, midx):
-#     if dir == 1:  # 동
-#         while arr[nidx][midx+1] != '#' and (midx+1) < m:
-#             midx += 1
-#             if arr[nidx][midx] == '0':
-#                 return 1
-#     elif dir == 2:  # 서
-#         while arr[nidx][midx-1] != '#' and (midx-1) >= 0:
-#             midx -= 1
-#             if arr[nidx][midx] == '0':
-#                 return 1
-#     elif dir == 3:  # 남
-#         while arr[nidx+1][midx] != '#' and (nidx+1) < n:
-#             nidx += 1
-#             if arr[nidx][midx] == '0':
-#                 return 1
-#     elif dir == 4:  # 북
-#         while arr[nidx-1][midx] != '#' and (nidx-1) >= 0:
-#             nidx -= 1
-#             if arr[nidx][midx] == '0':
-#                 return 1
+            ri = nri
+            rj = nrj
+            while 0 <= ri + i[0] < n and 0 <= rj + i[1] < m and arr[ri+i[0]][rj+i[1]] != '#' and arr[ri+i[0]][rj+i[1]] != 'B' and visited[ri+i[0]][rj+i[1]] != 1:
+                ri += i[0]
+                rj += i[1]
+                visited[ri][rj] = 1
+                mark = True
+                if arr[ri][rj] == 'O':
+                    return cnt
+            q.append((ri, rj))
+        if mark:
+            cnt += 1
+        mark = False
 
+    return -1
 
 n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+arr = [list(map(str, input())) for _ in range(n)]
+
+visited = [[0] * m for _ in range(n)]
 
 for i in range(n):
     for j in range(m):
-        if arr[i][j] == 'R' or arr[i][j] == 'B':
-            move()
+        if arr[i][j] == 'R':
+            ridx = (i, j)
+        elif arr[i][j] == 'B':
+            bidx = (i, j)
 
+print(bfs(ridx, bidx))
