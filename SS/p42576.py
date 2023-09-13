5c469420ee3c0dddde52731f6dbd34921fbe65a6
jyeon <jeongyeon865@gmail.com>
2023-09-13 09:35:44 +0900

    [ADD] 09/13

---
+++ SS/p42576.py
@@ -0,0 +1,15 @@
+def solution(participant, completion):
+    d = {}
+    sumHash = 0
+
+    for i in participant:
+        d[hash(i)] = i
+        sumHash += hash(i)
+
+    for i in completion:
+        sumHash -= hash(i)
+
+    return d[sumHash]
+
+
+print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
