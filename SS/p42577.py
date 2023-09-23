a6ee24a7055bc09dbb719033f54e8029e26565b7
jyeon <jeongyeon865@gmail.com>
2023-09-23 10:14:53 +0900

    [ADD] 09/19

--- SS/p42577.py
+++ SS/p42577.py
@@ -2,14 +2,11 @@ def solution(phone_book):
     answer = True
     phone_book.sort()
     for i in range(len(phone_book) - 1):
-        for j in range(i+1, len(phone_book)):
-            if phone_book[i][0] != phone_book[j][0]:
-                break
-            elif phone_book[i][0] == phone_book[j][0] and phone_book[i] in phone_book[j]:
-                    return False
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
     return answer
 
 
-print(solution(["119", "97674223", "1195524421"]))
-print(solution(["456","789", "123"]))
-print(solution(["123","1235","567","88","12"]))
+print(solution(["119", "97674223", "1195524421"]))  # false
+print(solution(["456", "789", "123"]))  # true
+print(solution(["123", "1235", "567", "88", "12"]))  # false
