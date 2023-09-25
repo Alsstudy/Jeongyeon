57bf2cfe6b5182610afb583322c51141aa1d9f87
jyeon <jeongyeon865@gmail.com>
2023-09-25 15:27:39 +0900

    [ADD] 09/25

--- SS/n14500.py
+++ SS/n14500.py
@@ -1,44 +1,49 @@
-def move(cnt, x, y, val):
+def dfs(x, y, cnt, sum):
     global maxVal
+
     if cnt == 4:
-        maxVal = max(maxVal, val)
+        maxVal = max(maxVal, sum)
         return
 
-    if not visited[x][y]:
-        for i in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            if 0 <= x + i[0] < n and 0 <= y + i[1] < m:
-                visited[x][y] = True
-                move(cnt+1, x + i[0],  y + i[1], val+arr[x][y])
+    for i in move:
+        nx = x + i[0]
+        ny = y + i[1]
+        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+            visited[nx][ny] = True
+            dfs(nx, ny, cnt+1, sum+arr[nx][ny])
+            visited[nx][ny] = False
 
 
-def check():
+def exce(x, y):
     global maxVal
-    for j in range(m-2):
-        maxVal = max(sum(arr[0][j:j+3]) + arr[1][j+1], maxVal)
-        maxVal = max(sum(arr[n-1][j:j+3]) + arr[n-2][j+1], maxVal)
 
-    for i in range(1, n-1):
-        for j in range(m-2):
-            maxVal = max(sum(arr[i][j:j+3]) + max(arr[i-1][j+1], arr[i+1][j+1]), maxVal)
+    for i in range(4):
+        sum = arr[x][y]
+        for j in range(3):
+            idx = (i+j) % 4
+            nx = x + move[idx][0]
+            ny = y + move[idx][1]
+            if 0 <= nx < n and 0 <= ny < m:
+                sum += arr[nx][ny]
+            else:
+                sum = 0
+                break
 
-    for i in range(n-2):
-        maxVal = max(arr[i][0] + arr[i+1][0], arr[i+2][0] + arr[i+1][1], maxVal)
-        maxVal = max(arr[i][m-1] + arr[i+1][m-1] + arr[i+2][m-1] + arr[i+1][m-1], maxVal)
-
-    for i in range(n-2):
-        for j in range(1, m-1):
-            maxVal = max(arr[i][j] + arr[i+1][j] + arr[i+2][j] + max(arr[i+1][j-1], arr[i+1][j+1]), maxVal)
+        maxVal = max(maxVal, sum)
 
 
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-maxVal = arr[0][0]
+
+maxVal = 0
+move = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+visited = [[0] * m for _ in range(n)]
 
 for i in range(n):
     for j in range(m):
-        visited = [[False] * m for _ in range(n)]
-        move(0, i, j, 0)
-
-check()
+        visited[i][j] = True
+        dfs(i, j, 1, arr[i][j])
+        visited[i][j] = False
+        exce(i, j)
 
 print(maxVal)
